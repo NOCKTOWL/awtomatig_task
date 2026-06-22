16d46e3da4cb8a2def1cb0f176dfa06b8b275fe7
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2026-06-23 04:05:09 +0600

    promise fix

--- client/app/api/tasks/[id]/route.ts
+++ client/app/api/tasks/[id]/route.ts
@@ -1,9 +1,9 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+export async function PATCH(request: Request, { params }: { params: Promise<{ id: string }> }) {
     try {
-        const { id } = params;
+        const { id } = await params;
         const { status } = await request.json();
         const task = await prisma.task.update({
             where: { id },
